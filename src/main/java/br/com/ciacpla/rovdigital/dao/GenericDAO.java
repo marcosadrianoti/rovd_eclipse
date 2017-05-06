89ff520eb228f62303c962b88016ac1dbfb216e1
Marcos Adriano TI <marcosadriano.ti@gmail.com>
2017-05-06 11:31:02 -0300

    GenericDAO - Listar, buscar e excluir funcionando.

--- src/main/java/br/com/ciacpla/rovdigital/dao/GenericDAO.java
+++ src/main/java/br/com/ciacpla/rovdigital/dao/GenericDAO.java
@@ -1,9 +1,12 @@
 package br.com.ciacpla.rovdigital.dao;
 
 import java.lang.reflect.ParameterizedType;
+import java.util.List;
 
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
 
 import br.com.ciacpla.rovdigital.util.HibernateUtil;
 
@@ -33,6 +36,53 @@ public class GenericDAO<Entidade> {
 		}finally {
 			sessao.close();
 		}
-			
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Entidade> listar(){
+		Session sessao = HibernateUtil.getFabricaDeSessoes().openSession();
+		try{
+			@SuppressWarnings("deprecation")
+			Criteria consulta = sessao.createCriteria(classe);
+			List<Entidade> resultado = consulta.list();
+			return resultado;
+		}catch(RuntimeException erro){
+			throw erro;
+		}finally {
+			sessao.close();
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public Entidade buscar(Long codigo){
+		Session sessao = HibernateUtil.getFabricaDeSessoes().openSession();
+		try{
+			@SuppressWarnings("deprecation")
+			Criteria consulta = sessao.createCriteria(classe);
+			consulta.add(Restrictions.idEq(codigo));
+			Entidade resultado = (Entidade) consulta.uniqueResult();
+			return resultado;
+		}catch(RuntimeException erro){
+			throw erro;
+		}finally {
+			sessao.close();
+		}
+	}
+	
+	public void excluir(Entidade entidade) {
+		Session sessao = HibernateUtil.getFabricaDeSessoes().openSession();
+		Transaction transacao = null;
+		try{
+			transacao = sessao.beginTransaction();
+			sessao.delete(entidade);
+			transacao.commit();
+		}catch(RuntimeException erro){
+			if(transacao != null){
+				transacao.rollback();
+			}
+			throw erro;
+		}finally {
+			sessao.close();
+		}
 	}
 }
--- src/test/java/br/com/ciacpla/rovdigital/dao/StateDAOTest.java
+++ src/test/java/br/com/ciacpla/rovdigital/dao/StateDAOTest.java
@@ -1,17 +1,59 @@
 package br.com.ciacpla.rovdigital.dao;
 
+import java.util.List;
+
+import org.junit.Ignore;
 import org.junit.Test;
 
 import br.com.ciacpla.rovdigital.entity.State;
 
 public class StateDAOTest {
 	@Test
+	@Ignore
 	public void salvar() {
 		State estado = new State();
 		estado.setName("Distrito Federal");
 		estado.setAcronym("DF");
-		
+
 		StateDAO estadoDAO = new StateDAO();
 		estadoDAO.salvar(estado);
+	}
+
+	@Test
+	@Ignore
+	public void listar() {
+		StateDAO estadoDAO = new StateDAO();
+		List<State> resultado = estadoDAO.listar();
+
+		System.out.println("Encontrados: " + resultado.size());
+	}
+
+	@Test
+	@Ignore
+	public void buscar() {
+		long codigo = 1L;
+		StateDAO estadoDAO = new StateDAO();
+		State estado = estadoDAO.buscar(codigo);
+
+		if (estado == null) {
+			System.out.println("Nenhum registro encontrado.");
+		} else {
+			System.out.println(estado.getCodigo() + " - " + estado.getName());
+		}
+	}
+	
+	@Test
+	public void excluir(){
+		long codigo = 4L;
+		StateDAO estadoDAO = new StateDAO();
+		State estado = estadoDAO.buscar(codigo);
+
+		if (estado == null) {
+			System.out.println("Nenhum registro excluido.");
+		} else {
+			estadoDAO.excluir(estado);
+			System.out.println("Registro excluido:");
+			System.out.println(estado.getCodigo() + " - " + estado.getName());
+		}
 	}
 }
