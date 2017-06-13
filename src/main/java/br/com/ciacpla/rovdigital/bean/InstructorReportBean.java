beb37117987fe3d55d5c09a97f858c496ef20325
Marcos Adriano TI <marcosadriano.ti@gmail.com>
2017-06-12 21:16:45 -0300

    relatorio instrutores funcionanado.

---
+++ src/main/java/br/com/ciacpla/rovdigital/bean/InstructorReportBean.java
@@ -0,0 +1,117 @@
+package br.com.ciacpla.rovdigital.bean;
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import org.omnifaces.util.Faces;
+import org.omnifaces.util.Messages;
+
+import br.com.ciacpla.rovdigital.dao.PilotDAO;
+import br.com.ciacpla.rovdigital.entity.Pilot;
+import br.com.ciacpla.rovdigital.util.HibernateUtil;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperPrintManager;
+
+@SuppressWarnings("serial")
+@ManagedBean
+@ViewScoped
+public class InstructorReportBean implements Serializable {
+
+	// MODELOS
+	private Pilot pilot;
+	private List<Pilot> pilots;
+	private Date dataInicial;
+	private Date dataFinal;
+
+	public Pilot getPilot() {
+		return pilot;
+	}
+
+	public void setPilot(Pilot pilot) {
+		this.pilot = pilot;
+	}
+
+	public List<Pilot> getPilots() {
+		return pilots;
+	}
+
+	public void setPilots(List<Pilot> pilots) {
+		this.pilots = pilots;
+	}
+
+	public Date getDataInicial() {
+		return dataInicial;
+	}
+
+	public void setDataInicial(Date dataInicial) {
+		this.dataInicial = dataInicial;
+	}
+
+	public Date getDataFinal() {
+		return dataFinal;
+	}
+
+	public void setDataFinal(Date dataFinal) {
+		this.dataFinal = dataFinal;
+	}
+
+	// CONTROLES
+
+	public void gerarRelatorio() {
+		try {
+
+			String caminho = Faces.getRealPath("/reports/instructors.jasper");
+
+			Date dataInicio = getDataInicial();
+			Date dataFinal = getDataFinal();
+
+			String invaName;
+			if (pilot == null) {
+				invaName = "%%";
+			} else {
+				invaName = pilot.getName();
+			}
+
+			Map<String, Object> parametros = new HashMap<>();
+			parametros.put("DATA_INICIAL", dataInicio);
+			parametros.put("DATA_FINAL", dataFinal);
+			parametros.put("INSTRUCTOR", invaName);
+
+			Connection conexao = HibernateUtil.getConexao();
+
+			JasperPrint relatorio = JasperFillManager.fillReport(caminho, parametros, conexao);
+//			JasperExportManager.exportReportToXml(relatorio,true);
+			
+			JasperPrintManager.printReport(relatorio, true);
+
+		} catch (JRException erro) {
+			Messages.addGlobalError("Ocorreu um erro ao tentar gerar o Relatório");
+			erro.printStackTrace();
+		}
+	}
+
+	@PostConstruct // Executa este controle logo que o managebean for criado.
+	public void novoRelatorio() {
+		this.setPilot(null);
+		this.setDataInicial(null);
+		this.setDataFinal(null);
+		try {
+			PilotDAO pilotDAO = new PilotDAO();
+			pilots = pilotDAO.listarPilotoPeloTipo("invaFliesAtSchool");
+		} catch (RuntimeException erro) {
+			Messages.addFlashGlobalError("Erro ao tentar listar");
+			erro.printStackTrace();
+		}
+	}
+
+}
--- src/main/java/br/com/ciacpla/rovdigital/dao/PilotDAO.java
+++ src/main/java/br/com/ciacpla/rovdigital/dao/PilotDAO.java
@@ -1,7 +1,46 @@
 package br.com.ciacpla.rovdigital.dao;
 
-import br.com.ciacpla.rovdigital.entity.Pilot;
+import java.util.List;
 
-public class PilotDAO extends GenericDAO<Pilot>{
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+
+import br.com.ciacpla.rovdigital.entity.Pilot;
+import br.com.ciacpla.rovdigital.util.HibernateUtil;
+
+public class PilotDAO extends GenericDAO<Pilot> {
+	
+	@SuppressWarnings("unchecked")
+	public List<Pilot> listarPilotoPeloTipo(String tipo) {
+		Session sessao = HibernateUtil.getFabricaDeSessoes().openSession();
+		try {
+			@SuppressWarnings("deprecation")
+			Criteria consulta = sessao.createCriteria(Pilot.class);
+			consulta.add(Restrictions.eq(tipo,true));
+			List<Pilot> resultado = consulta.list();
+			return resultado;
+		} catch (RuntimeException erro) {
+			throw erro;
+		} finally {
+			sessao.close();
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Pilot> listarAlunos() {
+		Session sessao = HibernateUtil.getFabricaDeSessoes().openSession();
+		try {
+			@SuppressWarnings("deprecation")
+			Criteria consulta = sessao.createCriteria(Pilot.class);
+			consulta.add(Restrictions.eq("student",true));
+			List<Pilot> resultado = consulta.list();
+			return resultado;
+		} catch (RuntimeException erro) {
+			throw erro;
+		} finally {
+			sessao.close();
+		}
+	}
 
 }
